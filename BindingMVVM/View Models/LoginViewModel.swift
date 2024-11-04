cae1a1eba6faabd8f116d1cc75a826f0ec0235a4
Christian Santiago Vera Rojas <christian.vera@globant.com>
2024-11-04 14:48:23 -0500

    asde

---
+++ BindingMVVM/View Models/LoginViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  LoginViewModel.swift
+//  BindingMVVM
+//
+//  Created by Mohammad Azam on 1/1/22.
+//
+
+import Foundation
+
+struct LoginViewModel {
+    var username: String = ""
+    var password: String = ""
+}
